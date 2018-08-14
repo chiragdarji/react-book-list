723fdc01528dfaf29f75641f613c4c0c84c6643f
chiragdarji <chiragdarji.savli@gmail.com>
2018-08-14 17:09:33 +0530

    book list container creation

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        { title: 'Javascript: The good part' },
+        { title: 'Harry Potter'},
+        { title: 'The Dark Tower'},
+        { title: 'Eloaquet JS'},
+        { title: 'Head First JS'},
+    ]
+};
